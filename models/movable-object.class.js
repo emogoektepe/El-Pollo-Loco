233d897d8b3ee21eed0a47de0f536aed3930151f
emogoektepe <e.goektepe@googlemail.com>
2023-10-16 19:46:30 +0200

    added classes

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,15 +1,21 @@
 class MovableObject {
-    x;
-    y;
+    x = 120;
+    y = 250;
     img;
+    height = 150;
+    width = 100;
 
-    constructor(x, y, img) {
-        this.x = x;
-        this.y = y;
-        this.img = img;
+
+    loadImage(path) {
+        this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
+        this.img.src = path;
     }
 
     moveRight() {
         console.log('Moving right');
+    }
+
+    moveLeft() {
+        console.log('Moving left');
     }
 }
