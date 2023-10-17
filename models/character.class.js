e18095f77755dfbe2689695c7bf0eccb1d018d4b
emogoektepe <e.goektepe@googlemail.com>
2023-10-17 17:53:21 +0200

    added animations

--- models/character.class.js
+++ models/character.class.js
@@ -1,8 +1,9 @@
 class Character extends MovableObject {
     x = 100;
-    y = 180;
+    y = 190;
     height = 250;
     width = 100;
+    speed = 2;
 
     IMAGES_WALKING = [
         '../img/2_character_pepe/2_walk/W-21.png',
@@ -12,7 +13,9 @@ class Character extends MovableObject {
         '../img/2_character_pepe/2_walk/W-25.png',
         '../img/2_character_pepe/2_walk/W-26.png',
     ]
+    world;
 
+    walking_sound = new Audio('../audio/walk.mp3');
     constructor() {
         super().loadImage('../img/2_character_pepe/1_idle/idle/I-1.png');
         this.loadImages(this.IMAGES_WALKING);
@@ -20,12 +23,31 @@ class Character extends MovableObject {
     }
 
     animate() {
+        this.walking_sound.volume = 0.1;
         setInterval(() => {
-            let path = this.IMAGES_WALKING[this.currentImage];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-            if (this.currentImage >= this.IMAGES_WALKING.length) {
-                this.currentImage = 0;
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+                this.x += this.speed;
+                this.direction = false;
+                this.walking_sound.play();
+            }
+            if (this.world.keyboard.LEFT && this.x > 0) {
+                this.x -= this.speed;
+                this.direction = true;
+                this.walking_sound.play();
+            }
+            this.world.camera_x = -this.x + 100;
+        }, 1000 / 360);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                let path = this.IMAGES_WALKING[this.currentImage];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+                if (this.currentImage >= this.IMAGES_WALKING.length) {
+                    this.currentImage = 0;
+                }
+            } else {
+                this.loadImage('../img/2_character_pepe/1_idle/idle/I-1.png');
             }
         }, 100);
     }
