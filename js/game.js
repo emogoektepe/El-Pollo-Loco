ca50b744bf34e8cb293ed2fe1aeb38ca00a4b4ab
emogoektepe <e.goektepe@googlemail.com>
2023-12-09 15:16:21 +0100

    new begin

--- js/game.js
+++ js/game.js
@@ -1,46 +1,12 @@
 let canvas;
-let world;
-let keyboard = new Keyboard();
+let ctx;
+let character = new MovableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas, keyboard);
-    console.log(world.character);
+    ctx = canvas.getContext('2d');
+
+
+    console.log('My Character is', character);
+    
 }
-
-window.addEventListener('keydown', (e) => {
-    if (e.keyCode == 39) {
-        keyboard.RIGHT = true;
-    }
-    if (e.keyCode == 37) {
-        keyboard.LEFT = true;
-    }
-    if (e.keyCode == 38) {
-        keyboard.UP = true;
-    }
-    if (e.keyCode == 40) {
-        keyboard.DOWN = true;
-    }
-    if (e.keyCode == 32) {
-        keyboard.SPACE = true;
-    }
-
-});
-window.addEventListener('keyup', (e) => {
-    if (e.keyCode == 39) {
-        keyboard.RIGHT = false;
-    }
-    if (e.keyCode == 37) {
-        keyboard.LEFT = false;
-    }
-    if (e.keyCode == 38) {
-        keyboard.UP = false;
-    }
-    if (e.keyCode == 40) {
-        keyboard.DOWN = false;
-    }
-    if (e.keyCode == 32) {
-        keyboard.SPACE = false;
-    }
-
-});
---
+++ models/movableObject.class.js
@@ -0,0 +1,16 @@
+class MovableObject {
+    x = 120;
+    y = 400;
+    img;
+
+    constructor() {
+
+    }
+
+    moveRight() {
+        console.log('Movin Right!');
+    }
+}
+
+
+// VIDEO 06 - Klassen (11:47 MIN) <= teachable VIDEO dev Ak
