5b8e4114179d9647997dcef565995b46c68d862e
emogoektepe <e.goektepe@googlemail.com>
2023-12-11 11:23:51 +0100

    created enemies and background

---
+++ models/charakter.class.js
@@ -0,0 +1,15 @@
+class Character extends MovableObject{
+
+    height = 300;
+    width = 150;
+    y = 150;
+
+    constructor() {
+        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');   
+    }
+
+    jump() {
+
+    }
+
+}
--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -1,16 +1,24 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 300;
+    height = 150;
+    width = 100;
     img;
 
     constructor() {
 
     }
 
+    loadImage(path) {
+        this.img = new Image(); // equals to // this.img = document.getElementById('image') <img id='image' src>
+        this.img.src = path;
+    }
+
     moveRight() {
-        console.log('Movin Right!');
+        console.log('Movin Right');
+    }
+
+    moveLeft() {
+        console.log('Movin Left');
     }
 }
-
-
-// VIDEO 06 - Klassen (11:47 MIN) <= teachable VIDEO dev Ak
